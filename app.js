9e975c69918a09de47f6d5220355c1cc7b639dc2
Muhammad-Asif-Ansari <muhammadasifansari101@gmail.com>
2024-09-26 23:20:16 +0500

    Assignment JavaScript
    Assignment JavaScript

---
+++ app.js
@@ -0,0 +1,243 @@
+//ASSIGNMENT 02;
+//CHAPTER 03;
+//QUESTION 01
+//var age = "I am " + 15 + " years old";
+//alert(age);
+
+//QUESTION 02
+// var num = 1;
+// alert(num++);
+// alert(num++);
+// alert(num++);
+// alert(num++);
+// alert(num++);
+// alert(num++);
+// alert(num++);
+// alert(num++);
+// alert(num++);
+// alert(num++);
+// alert(num++);
+// alert(num++);
+// alert(num++);
+// alert(num++);
+
+//QUESTION 03
+//var birthYear ="My birth year is " + 2002;
+//document.write(birthYear);
+
+
+//QUESTION 04
+//var vistorName = "John Doe";
+//var productTitle = "XYZ Clothing Store";
+//var quantity = "5 T-shirt";
+//document.write("vistorName: "+ "John Doe");
+//document.write("<br>productTitle: "+ "XYZ Clothing Store");
+//document.write("<br>quantity: "+ "5 T-shirt");
+
+//ASSIGNMENT 02:
+//CHAPTER 04:
+//QUESTION 01
+//var num1 = 5, name1 = "Iqbal",  age = 25;
+//console.log(num1,name1,age);
+
+//QUESTION 02:
+//LEGAL & ILLEGAL VARIABLE NAME:
+        //(1)
+//fullname =>  illegal
+//fullName =>  legal
+        //(2)
+//2prize =>   illegal
+//prize2 => legal
+        //(3)
+//your-First-Name => illegal
+//your_first_name =>  legal
+        //(4)
+//$rollno =>  illegal
+//rollNo =>  legal
+        //(5)
+//_first_name => illegal
+//first_name =>  legal
+
+//QUESTION  03:
+
+
+
+
+//CHAPTER 5:
+//QUESTION 01:
+//var value1 = 3;
+//var value2 = 5;
+//var result = value1 + value2;
+//document.write(result);
+
+//QUESTION  02:
+//var num1 = 9;
+//var num2 = 6;
+//var result = num1 - num2;
+//console.log(result);
+
+//var num1 = 9;
+//var num2 = 6;
+//var result = num1 * num2;
+//console.log(result);
+
+//var num1 = 9;
+//var num2 = 6;
+//var result = num1 / num2;
+//console.log(result);
+
+
+//var num1 = 9;
+//var num2 = 6;
+//var result = num1 % num2;
+//console.log(result);
+
+//QUESTION  03:
+// var number;
+// number = 5;
+// var result = (number++ + number+7 );
+
+// console.log(result);
+
+
+
+//QUESTION  04:
+//var ticket = 5;
+//var price = 600;
+//var totalCost = ticket * price;
+//var  totalCost = "Total cost to  buy " + ticket + " to movie is " + totalCost + "PKR";
+//document.write(totalCost);
+
+
+//QUESTION 05:
+//var n = 4;
+//for(var  i = 1 ;  i<=10 ; ++i)
+//console.log(n + " * " + i + " = " + n * i);
+
+//QUESTION 07:
+//var priceOfItem1 = 650;
+//var quantityOfItem1 = 3;
+//var priceOfItem2 = 100;
+//var quantityOfItem2 = 7;
+//var shippingCharges  = 100;
+
+//var totalCost = (priceOfItem1 * quantityOfItem1) + (priceOfItem2  * quantityOfItem2) + (shippingCharges);
+//console.log("Total cost of your order is " + totalCost + "PKR");
+
+//QUESTION  08:
+//var totalMarks = 980;
+//var obtainedMarks = 804;
+//var percentage = (obtainedMarks/totalMarks)*100
+//console.log("Percentage is " + percentage + "%");
+
+//QUESTION  09:
+//var usDollar = 10;
+//var pakistaniRupee1 = 104.80;
+//var saudiRiyals = 25;
+//var pakistaniRupee2 = 28;
+//var total = (usDollar * pakistaniRupee1) + (saudiRiyals * pakistaniRupee2);
+//document.write("Total Currency in PKR :" + total);
+
+//QUESTION  10:
+//var num1 = 8;
+//var num2 = 5
+//var num3 = 10;
+//var num4 = 2;
+//var result = (((num1 + num2) * num3) / num4);
+//document.write(result);
+
+//QUESTION   11:
+//var currentYear = 2024;
+//var birthYear = 2002;
+//var age = currentYear - birthYear;
+//document.write("They are either " + age + " years old" );
+
+//QUESTION 12;
+//var radius = 20;
+//var circumferenceOfCircle = 2*3.14 *radius;
+//var  areaOfCircle = 3.14 * radius * radius ;
+//document.write("The radius of a circle is " + radius);
+//document.write("<br>The  circumference of a circle is: " +circumferenceOfCircle);
+//document.write("<br> The area of a circle is: " + areaOfCircle);
+
+//QUESTION 13;
+//var snack = "chips";
+//var age = 12;
+//var maximumAge = 60;
+//var snacksPerDay = 5;
+//var  totalSnacks = (maximumAge - age) * 5;
+
+//document.write("<h1>The Lifetime Supply Calculator</h1>");``
+//document.write("Favourite snack: " + snack );
+//document.write("<br>Current age: " + age);
+//document.write("<br>Estimated maximum age: " + maximumAge);
+//document.write("<br>Amounts of snacks per day: " + snacksPerDay);
+//document.write("<br>You will need " + totalSnacks + " chips to last you until the ripe old age of " + maximumAge);
+
+
+//CHAPTER 6-9:
+
+//QUESTION 01:
+//var a = 10;
+//document.write("The value of a is: " + a);
+//++a; 
+//document.write("<br>Now the value of a is: " + a);
+//a++;
+//document.write("<br>Now the value of a is: " + a);
+//--a;
+//document.write("<br>Now the value of a is: " + a);
+//a--;
+//document.write("<br>Now the value of a is: " + a);
+
+
+//QUESTION 02:
+//var a = 2;
+//var b = 1;
+// var value1 = --a; //1
+// var value2 = --a - --b; //0
+// var value3 = --a - --b + ++b; //2
+//var result = --a - --b + ++b + b--; //3
+//document.write("<br>" + result);
+
+//QUESTION 03:
+// var greet = prompt("Enter your Name");
+// document.write("Hello Every One");
+
+//QUESTION 04:
+//var displayTable = prompt("Enter a Number");
+//if(displayTable == ""){
+  //      table = 5
+//}
+//for(var i = 1; i <= 10 ; i++){
+  //      document.write(displayTable + " * " + i + " = " + displayTable * i  + "<br>"); 
+//}
+
+// var subject1 = prompt("Enter Subject1");
+// var obtMarks1 = prompt("Enter obtMarks in subject1 and out of 100");
+
+// var subject2 = prompt("Erter Subject2");
+// var obtMarks2 = prompt("Enter obtMarks in subject2 and out of 100");
+
+
+// var subject3 = prompt("Enter Subject3");
+// var obtMarks3 = prompt("Enter obtMarks in subject3 and out of 100");
+
+// var totalMarks = 100;
+// var subject1 = obtMarks/totalMarks * 100;
+// var subject2 = obtMarks/totalMarks * 100;
+// var subject3 = obtMarks/totalMarks * 100;
+
+// var totalObtMarks = obtMarks1 + obtMarks2 + obtMarks3;
+
+
+
+
+
+
+
+
+
+
+
+
+
